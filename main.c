289174402d747ad295a1e25b60841b9eefb5d367
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-07-27 13:45:01 +0530

    Seperated .vert and .frag files

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <input.h>
+#include <stdlib.h>
 
 #pragma region Variables
 unsigned int VAO, VBO;
@@ -17,62 +18,120 @@ static float vertices[] = {
 };
 #pragma endregion
 
-#pragma region Shader Source
+char* LoadShaderSource(const char* filepath) {
+    FILE* file = fopen(filepath, "rb");
+    if (!file) {
+        fprintf(stderr, "Error opening shader file: %s\n", filepath);
+        return NULL;
+    }
 
-// Vertex Shader Source
-const char* vertexShaderSource = "#version 330 core\n"
-"layout(location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos, 1.0);\n"
-"}\0";
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    rewind(file);
 
-// Fragment Shader Source
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
-"}\0";
+    if (length <= 0) {
+        fprintf(stderr, "Invalid shader file length: %ld\n", length);
+        fclose(file);
+        return NULL;
+    }
 
-#pragma endregion
+    char* buffer = (char*)malloc(length + 1);
+    if (!buffer) {
+        fprintf(stderr, "Memory allocation failed\n");
+        fclose(file);
+        return NULL;
+    }
+
+    size_t bytesRead = fread(buffer, 1, length, file);
+    if (bytesRead != length) {
+        fprintf(stderr, "Shader read incomplete: expected %ld, got %zu\n", length, bytesRead);
+        free(buffer);
+        fclose(file);
+        return NULL;
+    }
+
+    buffer[length] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+
+void CheckCompileErrors(unsigned int shader, const char* type)
+{
+	int success;
+	char infoLog[1024];
+	if (strcmp(type, "PROGRAM") != 0)
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			printf("ERROR::SHADER_COMPILATION_ERROR of type: %s\n%s\n", type, infoLog);
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			printf("ERROR::PROGRAM_LINKING_ERROR of type: %s\n%s\n", type, infoLog);
+		}
+	}
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void InitShaderProgram() {
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+void InitShaderProgram()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-	glBindVertexArray(VAO);
-    
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(VAO);
 
-	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Compile Vertex Shader
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-	// Compile Fragment Shader
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+    // Load Shaders
+    const char* vertexPath = "src/Standard.vert";
+    const char* fragmentPath = "src/Standard.frag";
 
-	// Link Shaders into Shader Program
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+    char* vertexShaderSource = LoadShaderSource(vertexPath);
+    char* fragmentShaderSource = LoadShaderSource(fragmentPath);
 
-	//Delete shaders as they're linked into our program now and no longer necessary
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+    if (!vertexShaderSource || !fragmentShaderSource) return;
+
+    // Compile Vertex Shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, (const char**)&vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    CheckCompileErrors(vertexShader, "VERTEX");
+
+    // Compile Fragment Shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, (const char**)&fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    CheckCompileErrors(fragmentShader, "FRAGMENT");
+
+    // Shader Program
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    CheckCompileErrors(shaderProgram, "PROGRAM");
+
+    // Clean up shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    free(vertexShaderSource);
+    free(fragmentShaderSource);
 }
 
 int main() {
